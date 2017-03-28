e20df02a6e9abbe75ab6a9b2cbee5df7647a5b71
Feña Agar <fernando.agar@gmail.com>
2017-03-28 13:10:51 +0200

    Check for different extensions

--- content.js
+++ content.js
@@ -1,9 +1,27 @@
+// TODO: Call this on hover or something like that.
+function isValidURL(url) {
+  var http = new XMLHttpRequest();
+
+  http.open('HEAD', url, false);
+  http.send();
+  return http.status != 404;
+}
+
 function addFullLink(){
   $("ul li figure").each(function(){
-    id  = $(this).attr('data-wallpaper-id');
-    url = `https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-${id}.jpg`
+    exts  = [".jpg",".png",".jpeg"];
+    id    = $(this).attr('data-wallpaper-id');
+    url   = `https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-${id}`
 
-    $(this).find('.wall-res').wrap('<a href='+ url +' target=_blank download></a>')
+    for ( var i in exts ) {
+      extURL = url + exts[i];
+
+      if ( isValidURL(extURL) ) {
+        console.log("Found " + extURL);
+        $(this).find('.wall-res').wrap('<a href='+ extURL +' target=_blank download></a>');
+        break;
+      };
+    };
   });
 };
 
